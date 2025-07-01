201b6d3703ba44e363955c53ce44550e24408714
Gabriel Torres <gabrielst328@gmail.com>
2025-07-01 18:34:01 -0300

    refac/feat: id is automatically setted by uuid v4 on instantiation, added dummy data

--- lib/data/list_items_dummy.dart
+++ lib/data/list_items_dummy.dart
@@ -1,1 +1,7 @@
-const listItemsDummy = [];
+import 'package:shopping_list/model/list_item_model.dart';
+
+final listItemsDummy = [
+  ListItemModel(name: 'Leite', category: ECategory.liquid, quantity: 1),
+  ListItemModel(name: 'Banana', category: ECategory.fruit, quantity: 5),
+  ListItemModel(name: 'Bife', category: ECategory.meal, quantity: 1),
+];
--- lib/model/list_item_model.dart
+++ lib/model/list_item_model.dart
@@ -1,7 +1,16 @@
+import 'package:uuid/uuid.dart';
+
+enum ECategory { liquid, fruit, candy, salad, meal }
+
 class ListItemModel {
-  ListItemModel({required this.id, required this.name, required this.category});
+  ListItemModel({
+    required this.name,
+    required this.category,
+    required this.quantity,
+  }) : id = Uuid().v4();
 
   final String id;
   final String name;
-  final String category;
+  final ECategory category;
+  final int quantity;
 }
